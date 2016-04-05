1dc7f627dfc1430b1187bcdbd1b5a2798d625685
amoblin@mbp <amoblin@gmail.com>
2016-04-05 17:42:50 +0800

    01. Two Number

--- MyLeetCode/main.swift
+++ MyLeetCode/main.swift
@@ -10,3 +10,19 @@ import Foundation
 
 print("Hello, World!")
 
+func twoNum(inputArray: [Int], sum: NSInteger) -> [Int]
+{
+//    var dict:NSMutableDictionary = [:]
+    let middle = Float(sum) / 2
+    var groupDict = [Float:Int]()
+    for (index, item) in inputArray.enumerate() {
+        let offset = Float(item) - middle
+        if groupDict.indexForKey(-offset) != nil {
+            return [groupDict[-offset]!, index]
+        }
+        groupDict[offset] = index
+    }
+    return []
+}
+
+print(twoNum([2, 13, 7, 9, 11], sum: 9))
